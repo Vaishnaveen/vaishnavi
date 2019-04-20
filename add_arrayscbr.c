fcf51ce7f4a6d6f061ec6c9a41244be13d1ffb4a
Vaishnaveen <49424725+Vaishnaveen@users.noreply.github.com>
2019-04-20 10:56:31 +0530

    Create add_arrayscbr.c

---
+++ add_arrayscbr.c
@@ -0,0 +1,38 @@
+#inlcude<stdio.h>
+void inputn(int *n)
+{
+    scanf("%d",n);
+    return;
+}
+void inputnumbers(int n, int a[])
+{   
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",a[i]);
+    }
+    return a[i];
+}
+void compute(int a[i], int s)
+{
+    
+    for(int i=0;i<n;i++)
+    {
+        s=s+a[i];
+    }
+    return s;
+}
+void output(int s)
+{
+    for(int i=0;i<n;i++)
+    {
+        printf("tne sum of n nos is:%d\n",s):
+    }
+}
+ int main()
+{
+    int i,n,a[i];
+    printf("read n:\n",n);
+    n=input(&n);
+    s=compute(s,a[i]);
+    s=output(s);
+}
