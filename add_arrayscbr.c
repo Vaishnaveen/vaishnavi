dac4dc252f7082fbf160ca531586b73811b9ed6f
Vaishnaveen <49424725+Vaishnaveen@users.noreply.github.com>
2019-04-20 12:26:28 +0530

    Update add_arrayscbr.c

--- add_arrayscbr.c
+++ add_arrayscbr.c
@@ -1,28 +1,29 @@
 #include<stdio.h>
-void inputn(int *x)
+void inputn(int *n)
 {
-    scanf("%d",x);
+    scanf("%d",n);
     return;
 }
 void inputnumbers(int n, int a[])
-{   
+{
     for(int i=0;i<n;i++)
     {
-        scanf("%d",a[i]);
+        scanf("%d",&a[i]);
     }
     return;
 }
-void compute(int a[n], int s)
+void compute(int a[], int s)
 {
-    
+    int n;
     for(int i=0;i<n;i++)
     {
-        s=s+a[i];
+        s=s+a[n];
     }
     return;
 }
 void output(int s)
 {
+    int n;
     for(int i=0;i<n;i++)
     {
         printf("tne sum of n nos is:%d\n",s);
@@ -30,11 +31,11 @@ void output(int s)
 }
  int main()
 {
-    int x,s,n;
-    printf("read n:\n",x);
-    input(&n);
+    int s,n;
+    printf("read n:\n",n);
+    inputn(&n);
     int a[n];
-    inputnumbers(n,&a[]);
-    compute(&s,&a[n]);
+    inputnumbers(n,&a[n]);
+    compute(&s,a[n]);// when the program expects an integer but in function call the given value is that of a pointer there is an error of making a pointer from integer without a cast//
     output(s);
 }
