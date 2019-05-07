44b3484a32bbe261d3c6538df18ea24225d9609b
Vaishnaveen <49424725+Vaishnaveen@users.noreply.github.com>
2019-05-07 12:30:07 +0530

    Update struct.c

--- struct.c
+++ struct.c
@@ -6,17 +6,16 @@ struct fraction
 };
 void getinput(int *x)
 {
-	scanf("%d",x);
+	scanf("%d/%d",x);
 	return;
 }
 int main()
 {
 	struct fraction x;
-	printf("Enter numerator:\n");
-	getinput(&x.nume);
-	printf("Enter denominator:\n");
-	getinput(&x.deno);
+	printf("Enter the numerator and denominator:\n");
+	getinput(&x)
 	printf("The fraction is %d/%d ",x.nume,x.deno);
 	return 0;
 }
 	
+//the input must be in a fractional form since we are already using composite structure//
