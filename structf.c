3f309df30a56e4d08a39194c7a35069eba534de4
Vaishnaveen <49424725+Vaishnaveen@users.noreply.github.com>
2019-05-20 09:30:41 +0530

    Update structf.c

--- structf.c
+++ structf.c
@@ -5,6 +5,11 @@ struct fraction
     int deno;
 };
 void getinput(struct fraction *x)
-{
-  
+{   
+    printf("Enter the numerator:\n");
+    scanf("%d"&x->nume);
+    printf("Enter the denominator:\n");
+    scanf("%d",&x->deno);
+    return;
+}
   
