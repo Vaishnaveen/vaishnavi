9b3304726c94ce4cef04dc8fc030fa64a8e65a77
Vaishnaveen <49424725+Vaishnaveen@users.noreply.github.com>
2019-05-21 09:26:54 +0530

    Update structf.c

--- structf.c
+++ structf.c
@@ -12,4 +12,20 @@ void getinput(struct fraction *x)
     scanf("%d",&x->deno);
     return;
 }
+void compute(struct fraction a,struct fraction b, struct fraction *c)
+{
+    fraction f1={.nume,b.deno};
+    fraction f2={b.nume,b.deno};
+    sum=={f1
+int main()
+{
+    struct fraction a,b,c;
+    printf("Enter the fraction1 in X/Y form:\n");
+    getinput(&x);
+    printf("Enter the fraction2 in X/Y form:\n");
+    getinput(&x);
+    compute(&a,&b,c);
+    output(&c);
+    return 0;
+}
   
