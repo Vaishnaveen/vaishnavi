a81c621a7b6df1b4a7d38db81f0ac1aafbb44ce8
Vaishnaveen <49424725+Vaishnaveen@users.noreply.github.com>
2019-07-23 14:43:05 +0530

    Update structf.c

--- structf.c
+++ structf.c
@@ -12,20 +12,38 @@ void getinput(struct fraction *x)
     scanf("%d",&x->deno);
     return;
 }
-void compute(struct fraction a,struct fraction b, struct fraction *c)
+struct fraction compute(struct fraction a,struct fraction b)
+{   
+    struct fraction c;
+    c.nume=(a.nume*b.deno)+(b.nume*a.deno);
+    c.deno=a.deno*b.deno;
+    return c;
+}
+struct fraction common(struct fraction c)
 {
-    fraction f1={a.nume,a.deno};
-    fraction f2={b.nume,b.deno};
-    sum  
+    int m,n,gcd=0;
+    m=c.nume;
+    n=c.deno;
+    while(m!=n)
+    {
+        if(m>n)
+            m-=n;
+        else
+            n-=m;
+    }
+    gcd=m;
+    c.nume=c.nume/gcd;
+    c.deno=c.deno/gcd;
+    return c;
+}
 int main()
 {
     struct fraction a,b,c;
-    printf("Enter the fraction1 in X/Y form:\n");
-    getinput(&x);
-    printf("Enter the fraction2 in X/Y form:\n");
-    getinput(&x);
-    compute(&a,&b,c);
-    output(&c);
+    a=getinput();
+    b=getinput();
+    c=compute(a,b);
+    c=common(c);
+    printf("The sum of fractions is %d/%d\n",c.nume,c.deno);
     return 0;
 }
   
